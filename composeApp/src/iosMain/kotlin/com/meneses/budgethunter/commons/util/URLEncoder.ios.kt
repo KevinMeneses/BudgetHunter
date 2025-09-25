6038e554760be9331ca32501fe1d823c87d0a297
Kevin Meneses <kevmeneses@gmail.com>
2025-09-24 23:51:02 -0500

    - implement URL encoding and decoding for iOS using UTF-8

--- composeApp/src/iosMain/kotlin/com/meneses/budgethunter/commons/util/URLEncoder.ios.kt
+++ composeApp/src/iosMain/kotlin/com/meneses/budgethunter/commons/util/URLEncoder.ios.kt
@@ -1,11 +1,53 @@
 package com.meneses.budgethunter.commons.util
 
+import kotlinx.cinterop.BetaInteropApi
+import platform.Foundation.NSString
+import platform.Foundation.create
+import platform.Foundation.stringByAddingPercentEncodingWithAllowedCharacters
+import platform.Foundation.stringByRemovingPercentEncoding
+import platform.Foundation.NSCharacterSet
+import platform.Foundation.NSMutableCharacterSet
+
+@OptIn(BetaInteropApi::class)
 actual fun encodeURL(string: String, encoding: String): String {
-    // TODO: use actual decoder for ios
-    return string
+    return when (encoding.uppercase()) {
+        "UTF-8", "UTF_8" -> {
+            val nsString = NSString.create(string = string)
+            // Create a character set that matches Java's URLEncoder behavior
+            // Java URLEncoder encodes everything except: alphanumeric, *, -, ., _
+            val allowedCharacterSet = NSMutableCharacterSet()
+            allowedCharacterSet.formUnionWithCharacterSet(NSCharacterSet.alphanumericCharacterSet())
+            allowedCharacterSet.addCharactersInString("*-._")
+
+            nsString.stringByAddingPercentEncodingWithAllowedCharacters(
+                allowedCharacterSet
+            ) ?: string
+        }
+        else -> {
+            // Fallback for other encodings - just encode with UTF-8
+            val nsString = NSString.create(string = string)
+            val allowedCharacterSet = NSMutableCharacterSet()
+            allowedCharacterSet.formUnionWithCharacterSet(NSCharacterSet.alphanumericCharacterSet())
+            allowedCharacterSet.addCharactersInString("*-._")
+
+            nsString.stringByAddingPercentEncodingWithAllowedCharacters(
+                allowedCharacterSet
+            ) ?: string
+        }
+    }
 }
 
+@OptIn(BetaInteropApi::class)
 actual fun decodeURL(string: String, encoding: String): String {
-    // TODO: use actual decoder for ios
-    return string
+    return when (encoding.uppercase()) {
+        "UTF-8", "UTF_8" -> {
+            val nsString = NSString.create(string = string)
+            nsString.stringByRemovingPercentEncoding ?: string
+        }
+        else -> {
+            // Fallback for other encodings
+            val nsString = NSString.create(string = string)
+            nsString.stringByRemovingPercentEncoding ?: string
+        }
+    }
 }
